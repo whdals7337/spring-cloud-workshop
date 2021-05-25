613396d1ee6992f1576fb6f41c679fb88e731ed8
wellbell <uok02018496@naver.com>
2021-05-25 10:59:15 +0900

    [feat] product 유레카 설정 spring cloud config server 를 통해서 설정되도록 수정

--- product/src/main/java/com/elevenst/api/ProductController.java
+++ product/src/main/java/com/elevenst/api/ProductController.java
@@ -1,13 +1,19 @@
 package com.elevenst.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Value("${config.test.str}")
+    public String test;
 
     @GetMapping(path = "{productId}")
     public String getProductInfo(@PathVariable String productId) {
@@ -17,7 +23,7 @@ public class ProductController {
 //            e.printStackTrace();
 //        }
 
-        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]";
+        return "[product id = " + productId + " at " + System.currentTimeMillis() + "]" + test;
 //        throw new RuntimeException("I/O Exception");
     }
 }
